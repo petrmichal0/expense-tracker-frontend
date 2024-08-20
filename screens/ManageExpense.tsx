d9e40a0362d2ce1f39c0a13dd3cbaa331d2a2611
Petr Michal <petrmichal0@gmail.com>
2024-08-20 21:18:17 +0200

    ADD The Starting Setup Navigation

---
+++ screens/ManageExpense.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function ManageExpense() {
+  return <Text>ManageExpense</Text>;
+}
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function RecentExpenses() {
+  return <Text>ResentExpenses</Text>;
+}
+
+export default RecentExpenses;
