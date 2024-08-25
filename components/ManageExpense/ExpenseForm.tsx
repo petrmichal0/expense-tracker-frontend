78fe5b6f2dd4f6a45ce3316c6f1e430ba05f4fd1
Petr Michal <petrmichal0@gmail.com>
2024-08-25 23:21:13 +0200

    Adding Validation

--- components/ManageExpense/ExpenseForm.tsx
+++ components/ManageExpense/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
 import { useState } from "react";
 
 import { GlobalStyles } from "../../constants/styles";
@@ -46,6 +46,16 @@ function ExpenseForm({
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input", "Please check your input values");
+      return;
+    }
+
     onSubmit(expenseData);
   }
 
